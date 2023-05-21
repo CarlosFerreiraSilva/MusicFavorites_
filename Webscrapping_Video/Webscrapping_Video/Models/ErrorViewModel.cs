20dbf620c62be313ab7599fe972978013edf4d44
CarlosFerreiraSilva <115711921+CarlosFerreiraSilva@users.noreply.github.com>
2023-05-21 10:12:10 -0300

    Atualização 1

---
+++ Webscrapping_Video/Webscrapping_Video/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Webscrapping_Video.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
